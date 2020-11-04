f51fa02236fe770379f9af3483296a596fbaa62f
Daniel <danielkolinek@seznam.cz>
2020-11-05 00:39:12 +0100

    python+nodejs fully functional

--- python+node/node_part/app.js
+++ python+node/node_part/app.js
@@ -1,15 +1,46 @@
-const tf = require('@tensorflow/tfjs-node');
+const tf = require('@tensorflow/tfjs-node-gpu');
 const bodyPix = require('@tensorflow-models/body-pix');
 const http = require('http');
-
-const net = bodyPix.load({
-    architecture: 'MobileNetV1',
-    outputStride: 16,
-    multiplier: 0.75,
-    quantBytes: 2
-});
-http.createServer(function(req, res){
-    res.writeHead(200, { "Content-Type": "text/plain" });
-
-    res.end("Hello World!\n");
-}).listen(8080, "127.0.0.1");
+(async () => {
+    //mobilnet
+    /* 
+    const net = await bodyPix.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        multiplier: 0.75,
+        quantBytes: 2
+    });
+    */
+   //resnet
+   /*
+    const net = await bodyPix.load({
+        architecture: "ResNet50",
+        quantBytes: 1,
+        outputStride: 16
+    });
+    */
+    const net = await bodyPix.load({
+        architecture: 'MobileNetV1',
+        outputStride: 16,
+        multiplier: 0.75,
+        quantBytes: 2
+    });
+    http.createServer(function(req, res){
+        var images = [];
+        req.on('data', (image) => {
+            images.push(image);
+        });
+        req.on('end', async () => {
+            const image = tf.node.decodeImage(Buffer.concat(images));
+            segmentation = await net.segmentPerson(image, {
+                flipHorizontal: false,
+                internalResolution: 'medium',
+                segmentationThreshold: 0.7,
+            });
+            res.writeHead(200, { 'Content-Type': 'application/octet-stream' });
+            res.write(Buffer.from(segmentation.data));
+            res.end();
+            tf.dispose(image);
+        });
+    }).listen(8080);;
+})();
--- python+node/remove_bg.py
+++ python+node/remove_bg.py
@@ -5,8 +5,9 @@ import argparse
 import numpy as np
 import cv2
 import argparse
+#custom imports
+from functions import run_video, run_img, run_camera
 
-"""
 # run like python3 morph.py --image_1 ../img/DB1_B/101_2.tif --image_2 ../img/DB1_B/102_2.tif --blocksize 10
 parser = argparse.ArgumentParser(
     description='Remove background from image or video using only python')
@@ -16,27 +17,49 @@ parser.add_argument("--img",
 parser.add_argument("--video",
                     metavar="/path/to/video", required=False,
                     help="Path to video which is containing bg that has to be removed.")
+parser.add_argument('--camerain', required=False,
+                    metavar="input device",
+                    help="input device, for example: /dev/video0")
+parser.add_argument('--cameraout', required=False,
+                    metavar="output device",
+                    help="output device, for example: /dev/video1")
+parser.add_argument("--width",
+                    metavar="camera width", required=False,
+                    help="For live camera input.")
+parser.add_argument("--height",
+                    metavar="camera height", required=False,
+                    help="For live camera input.")
+parser.add_argument("--fps",
+                    metavar="camera fps", required=False,
+                    help="For live camera input.")
 parser.add_argument('--bg', required=True,
                     metavar="/path/to/bg",
                     help="Backgroung that will replace oldone")
-parser.add_argument('--save', required=True,
+parser.add_argument('--save', required=False,
                     metavar="filename",
                     help="filename for result (example \"--save result.jpg\" will create result.jpg)")
 args = parser.parse_args()
+
+
+
+#if input is image
+if args.video is not None and args.bg is not None and args.save is not None:
+    frame_counter = 0
+    run_video(args.video, args.bg, args.save, frame_counter)
+
+elif args.img is not None and args.bg is not None and args.save is not None:
+    run_img(args.img, args.bg, args.save)
+
+elif args.camerain is not None and args.cameraout is not None:
+    run_camera(args.camerain, args.cameraout, args.height, args.width, args.fps, args.bg)
+
+else:
+    parser.print_help()
+
 """
-
-def get_mask(frame, bodypix_url='http://127.0.0.1'):
-    _, data = cv2.imencode(".jpg", frame)
-    r = requests.post(
-        url=bodypix_url,
-        data=data.tobytes(),
-        headers={'Content-Type': 'application/octet-stream'})
-    # convert raw bytes to a numpy array
-    # raw data is uint8[width * height] with value 0 or 1
-    mask = np.frombuffer(r.content, dtype=np.uint8)
-    mask = mask.reshape((frame.shape[0], frame.shape[1]))
-    return mask
-
 cap = cv2.VideoCapture('/dev/video0')
 success, frame = cap.read()
-cv2.imwrite("result",get_mask(frame))
+cv2.imshow("result.png",get_mask(frame)*255)
+cv2.waitKey(0)
+cv2.destroyAllWindows()
+"""
